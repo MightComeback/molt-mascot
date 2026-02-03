f981f806c4ed50ee00d07f58f9b64dadd873ae33
Ivan Kuznetsov <kuznetsovivan496@gmail.com>
2026-02-04 01:45:10 +0200

    fix: preload reads click-through env alias

--- apps/molt-mascot/src/preload.cjs
+++ apps/molt-mascot/src/preload.cjs
@@ -6,7 +6,7 @@ contextBridge.exposeInMainWorld('moltMascot', {
   setAlignment: (align) => ipcRenderer.send('molt-mascot:set-alignment', align),
   setOpacity: (opacity) => ipcRenderer.send('molt-mascot:set-opacity', opacity),
   setPadding: (padding) => ipcRenderer.send('molt-mascot:set-padding', padding),
-  onClickThrough: (callback) => ipcRenderer.on('molt-mascot:click-through', (_event, enabled) => callback(enabled)), 
+  onClickThrough: (callback) => ipcRenderer.on('molt-mascot:click-through', (_event, enabled) => callback(enabled)),
   onHideText: (callback) => ipcRenderer.on('molt-mascot:hide-text', (_event, hidden) => callback(hidden)),
   onReset: (callback) => ipcRenderer.on('molt-mascot:reset', () => callback()),
   platform: process.platform,
@@ -18,6 +18,7 @@ contextBridge.exposeInMainWorld('moltMascot', {
     idleDelayMs: process.env.MOLT_MASCOT_IDLE_DELAY_MS || '',
     errorHoldMs: process.env.MOLT_MASCOT_ERROR_HOLD_MS || '',
     hideText: process.env.MOLT_MASCOT_HIDE_TEXT || '',
-    clickThrough: process.env.MOLT_MASCOT_CLICKTHROUGH || '',
+    // Back-compat: accept both MOLT_MASCOT_CLICKTHROUGH and MOLT_MASCOT_CLICK_THROUGH
+    clickThrough: process.env.MOLT_MASCOT_CLICKTHROUGH || process.env.MOLT_MASCOT_CLICK_THROUGH || '',
   },
 });
